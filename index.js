7c5c210bea173fcb5d424e7885d379677396065b
William Viana <willdev.viana@gmail.com>
2022-05-12 11:45:18 -0300

    update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('First commit!')
+    res.send('First commit and first Deploy com Cyclic!')
 })
 app.listen(process.env.PORT || 3000)
